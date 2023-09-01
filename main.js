863add359d9e0806ae7f5787734eec573e228fe6
Medo El-Bahrawy <mohamedelbahrawy512@gmail.com>
2023-09-01 06:25:48 +0300

    Update

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 // Selectors
+let header = document.querySelector("header");
 let countQuestions = document.querySelector(".questionNo span");
 let start = document.querySelector(".start");
+let userName = document.querySelector(".user-input");
+let bodyHead = document.querySelector(".bodyHead");
 let questionDiv = document.querySelector(".question-div");
 let answerDiv = document.querySelector(".answers");
 let btn = document.querySelector(".sub");
@@ -8,11 +11,15 @@ let main = document.querySelector("main");
 let answerBtns = document.getElementsByName("answer");
 let userCounter = document.querySelector(".timeCounter");
 let finalResult = document.querySelector(".result span");
+const modeBtn = document.querySelector(".dark-mode");
+const body = document.querySelector("body");
+const getMode = localStorage.getItem("mode");
 
 // variables
 let countQ = 0;
 let score = 0;
 let countdownInterval;
+let user;
 
 // Main
 fetch("questions.json")
@@ -20,29 +27,37 @@ fetch("questions.json")
     return data.json();
   })
   .then((questions) => {
-    start.onclick = () => {
-      start.remove();
-      questionDiv.classList.add("question");
-      btn.style.display = "block";
-      setNoQuestions(questions.length);
-      setQuestions(questions[countQ], questions.length);
-      countdown(30, questions.length);
-      // when click next
-      btn.onclick = () => {
-        // Check if the answer is right and add to score
-        checkAnswer(questions[countQ]["right_answer"]);
-        // increment counter
-        countQ++;
-        // clear the question and answers
-        questionDiv.innerHTML = "";
-        answerDiv.innerHTML = "";
-        // set new question
-        setQuestions(questions[countQ], questions.length);
-        setNoQuestions(questions.length);
-        clearInterval(countdownInterval);
-        countdown(15, questions.length);
-        showResult(questions.length);
-      };
+    userName.onchange = () => {
+      if (userName === "") {
+        start.disable = true;
+      } else {
+        start.onclick = (e) => {
+          user = userName.value;
+          userName.remove();
+          start.remove();
+          questionDiv.classList.add("question");
+          btn.style.display = "block";
+          setNoQuestions(questions.length);
+          setQuestions(questions[countQ], questions.length);
+          countdown(60, questions.length);
+          // when click next
+          btn.onclick = () => {
+            // Check if the answer is right and add to score
+            checkAnswer(questions[countQ]["right_answer"]);
+            // increment counter
+            countQ++;
+            // clear the question and answers
+            questionDiv.innerHTML = "";
+            answerDiv.innerHTML = "";
+            // set new question
+            setQuestions(questions[countQ], questions.length);
+            setNoQuestions(questions.length);
+            clearInterval(countdownInterval);
+            countdown(60, questions.length);
+            showResult(questions.length);
+          };
+        };
+      }
     };
   });
 
@@ -55,6 +70,18 @@ function setQuestions(obj, num) {
   // create the question and answers elements
   if (countQ < num) {
     // if there is still questions
+    // add category
+    let category = document.createElement("span");
+    category.className = "category";
+    category.innerHTML = obj.category;
+    questionDiv.appendChild(category);
+    // create Photo
+    let picDiv = document.createElement("div");
+    picDiv.className = "img-container";
+    let pic = document.createElement("img");
+    pic.src = obj.image;
+    picDiv.appendChild(pic);
+    questionDiv.appendChild(picDiv);
     // create question
     let question = document.createElement("h3");
     let questionText = document.createTextNode(obj.title);
@@ -124,17 +151,61 @@ function showResult(num) {
     userCounter.remove();
     countQuestions.remove();
     let result;
+    let resultText;
     let div = document.createElement("div");
     div.className = "result";
+    // create Photo
+    let picDiv = document.createElement("div");
+    picDiv.className = "img-container";
+    let pic = document.createElement("img");
+    pic.src =
+      "https://s2.r29static.com/bin/entry/4bc/545x367,85/1347479/image.webp";
+    picDiv.appendChild(pic);
+    div.appendChild(picDiv);
     // check score
     if (score === num) {
-      result = `<span class="perfect">Perfect</span> You Got ${score} / ${num}`;
+      result = document.createElement("p");
+      resultText = document.createTextNode(
+        `Perfect ${user}, You Got ${score} / ${num}`
+      );
+      result.appendChild(resultText);
     } else if (score > num / 2 && score < num) {
-      result = `<span class="good">Good</span> You Got ${score} / ${num}`;
+      result = document.createElement("p");
+      resultText = document.createTextNode(
+        `Great ${user}, You Got ${score} / ${num}`
+      );
+      result.appendChild(resultText);
     } else {
-      result = `<span class="bad">Bad</span> You Got ${score} / ${num}`;
+      result = document.createElement("p");
+      resultText = document.createTextNode(
+        `Unfortunately ${user}, You Got ${score} / ${num}`
+      );
+      result.appendChild(resultText);
     }
-    div.innerHTML = result;
+    div.appendChild(result);
     main.appendChild(div);
   }
 }
+
+if (getMode === "dark") {
+  modeBtn.classList.add("active");
+  body.classList.add("dark");
+  header.classList.add("dark");
+  questionDiv.classList.add("dark");
+  btn.classList.add("dark");
+}
+
+modeBtn.addEventListener("click", () => {
+  // add class active for dark or remove for light
+  modeBtn.classList.toggle("active");
+  // dark mode for body
+  body.classList.toggle("dark");
+  header.classList.toggle("dark");
+  questionDiv.classList.toggle("dark");
+  btn.classList.toggle("dark");
+  // for local Storage
+  if (!body.classList.contains("dark")) {
+    return localStorage.setItem("mode", "light");
+  }
+  localStorage.setItem("mode", "dark");
+});
